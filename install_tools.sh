85444fe35e5d4b4ace276bbdbf2695b9fcf80a01
Trofogol <nickb.ufeim@gmail.com>
2021-04-03 09:35:26 +0300

    Create install_tools.sh

---
+++ install_tools.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# run this script as root (use sudo)
+
+# docker
+apt remove docker docker-engine docker.io containerd runc \
+&& \
+apt update && apt install -y \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg \
+lsb-release \
+&& \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg \
+&& \
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
+&& \
+apt update && apt install -y docker-ce docker-ce-cli containerd.io
+
+echo ""
+echo "########################################################"
+echo "################## DOCKER IS INSTALLED #################"
+echo "########################################################"
+echo ""
+
+# docker compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+&& \
+chmod +x /usr/local/bin/docker-compose
+
+echo ""
+echo "########################################################"
+echo "################# COMPOSE IS INSTALLED #################"
+echo "########################################################"
+echo ""
